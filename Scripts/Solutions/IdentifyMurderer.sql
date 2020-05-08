4e1b7a065802110387215c6142cab882ed433422
erika-e <erika.swartz@gmail.com>
2020-05-08 14:43:20 -0400

    Refactor to remove unnecessary CTE

--- Scripts/Solutions/IdentifyMurderer.sql
+++ Scripts/Solutions/IdentifyMurderer.sql
@@ -2,18 +2,15 @@
 --I heard a gunshot and then saw a man run out. He had a "Get Fit Now Gym" bag. The membership number on the bag started with "48Z". 
 --Only gold members have those bags. The man got into a car with a plate that included "H42W".
 
-WITH get_fit_jan9 AS (SELECT *
-FROM get_fit_now_check_in gfnci 
-LEFT JOIN get_fit_now_member m on gfnci.membership_id = m.id 
-where gfnci.check_in_date = '20180109' and m.id LIKE('%48Z%')
-)
 
 SELECT gf.check_in_date
 , gf.membership_id
-, gf.membership_status
-, gf.name
+, m.membership_status
+, m.name
 , p.license_id 
 , dl.plate_number 
-FROM get_fit_jan9 gf
-LEFT JOIN person p ON gf.person_id = p.id
+FROM get_fit_now_check_in gf
+LEFT JOIN get_fit_now_member m on gf.membership_id = m.id 
+LEFT JOIN person p ON m.person_id = p.id
 LEFT JOIN drivers_license dl on p.license_id = dl.id 
+where gf.check_in_date = '20180109' and m.id LIKE('%48Z%')
